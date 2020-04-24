a9e41b83541efce151697309921630d3ff97d2a5
ArtyomBagautdinov <bagautdinovartyom@gmail.com>
2020-04-24 17:41:25 +0200

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Your server listening on port 3000!');
+app.listen(5000, function () {
+  console.log('Your server listening on port 5000!');
 });
